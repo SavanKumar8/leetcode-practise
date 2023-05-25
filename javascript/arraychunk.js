c6feeed48352bcb9e56fa8866f140172e5c1e1d1
Savan Kumar <savansharma22@gmail.com>
2023-05-25 13:54:15 +0530

    array chunk

--- javascript/arraychunk.js
+++ javascript/arraychunk.js
@@ -1,8 +1,14 @@
 var chunk = function(arr, size) {
-    return arr.map((item,index)=>{
-        return arr.slice(index,index+size)
-    })
+    let result = []
+    let index=0
+    while (index<arr.length) {
+        result.push(arr.slice(index,index+size))
+        index +=size
+   
+    }
+    return result;
 };
 
+console.log(chunk([1,2,3,4,5],2))
 // Input: arr = [1,2,3,4,5], size = 1
-// Output: [[1],[2],[3],[4],[5]]
+// Output: [[1],[2],[3],[4],[5]] 
