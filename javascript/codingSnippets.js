841f4cd4d23cf111771b3ed3208cc1bce0e4de29
Savan Kumar <savansharma22@gmail.com>
2023-06-24 19:12:23 +0530

    js snippets

--- javascript/codingSnippets.js
+++ javascript/codingSnippets.js
@@ -93,3 +93,41 @@
 // }
 
 // bark.animal = 'dog';
+
+
+//<----------------------------------------->
+// function bark() {
+//   console.log("Woof!");
+// }
+
+// bark.animal = "dog";
+
+//<----------------------------------------->
+// function Person(firstName, lastName) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
+
+// const member = new Person("Lydia", "Hallie");
+// Person.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+
+// console.log(member.getFullName());
+
+//TypeError
+
+//<----------------------------------------->
+// All object have prototypes.
+// A: true
+// B: false
+// Answer
+// Answer: B
+// All objects have prototypes, except for the base object. 
+// The base object is the object created by the user, or
+//  an object that is created using the new keyword. 
+//  The base object has access to some methods and properties, 
+//  such as .toString. This is the reason why you can use built-in 
+//  JavaScript methods! All of such methods are available on the prototype. 
+//  Although JavaScript can't find it directly on your object, it goes down 
+//  the prototype chain and finds it there, which makes it accessible for you.
