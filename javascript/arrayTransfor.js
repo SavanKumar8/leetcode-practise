879a0a104f3d11e4570a1d3dda8c13f5b6823cbd
Savan Kumar <savansharma22@gmail.com>
2023-07-01 13:50:03 +0530

    map pollyfills

---
+++ javascript/arrayTransfor.js
@@ -0,0 +1,12 @@
+//2635. Apply Transform Over Each Element in Array
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = fn(arr[i], i);
+  }
+  return arr;
+};
