2ba75404034415605250a68a2f6aadbc88be2dad
Savan Kumar <savansharma22@gmail.com>
2023-09-19 18:13:45 +0530

    fibnocci series

---
+++ javascript/fibnoccii.js
@@ -0,0 +1,26 @@
+// input =  5
+
+//output = 0,1,1,2,3
+
+// 0, 1, n+1 =1 // 0+1
+//n = 1 n+2 =  3//0+2 
+//n-1 + 2-1 // +2-2 //1
+//3-1 +  3-2 // 2+1 = 3
+
+function fib(num) {
+    return num <= 1 ? num : fib(num-1)+fib(num-2) 
+}
+
+//console.log(fib(5))
+
+function fibnocci(num){
+    let n1 = 0;
+    let n2 =1;
+    for(let i =1;i<=num;i++){
+      console.log(n1)
+      nextItem = n1+n2
+      n1=n2;
+      n2=nextItem;
+    }
+}
+console.log(fibnocci(5));
